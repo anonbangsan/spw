bafaed6d28ba23d8c8587824bedc93d94d3b919d
anonbangsan <boy_anonbangsan@hotmail.com>
2015-04-23 19:35:51 +0700

    *08* เพิ่มในส่วนของ Menu ให้ผู้ใช้ ใช้งานง่ายขึ้น
    1.เมนู Main ประกอบไปด้วย 3 เมนูย่อย
    1.1 เมนู Restart คือ เมนูเริ่มใหม่
    1.2 เมนู Stop คือ เมนูหยุดเล่น
    1.3 เมนู Exit คือ เมนูปิดโปรแกรม
    
    2.เมนู About มีเพียง 1 เมนูย่อย
    2.1 เมนู Credit คือ เมนู แสดงป๊อปอัพว่าใครคือผู้พัฒนา

--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -1,8 +1,8 @@
-package f2.spw;
-
 import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
 	public static void main(String[] args){
@@ -10,14 +10,51 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
-		
-		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+                
+                /* *08* */
+                Font fn= new Font("Courier New", Font.BOLD,20);
+                JMenuBar menubar = new JMenuBar();
+                JMenu menuMain = new JMenu("Main");
+                JMenu menuAbout = new JMenu("About");
+                JMenuItem menuRestart =new JMenuItem("Restart");
+                JMenuItem menuExit = new JMenuItem("Exit");
+                JMenuItem menuStop = new JMenuItem("Stop");
+                JMenuItem menuCredit = new JMenuItem("Credit");      
+                menuMain.setFont(fn);
+                menuAbout.setFont(fn);
+                menuRestart.setFont(fn);
+                menuExit.setFont(fn);
+                menuStop.setFont(fn);
+                menuCredit.setFont(fn);
+                menuMain.add(menuRestart);
+                menuMain.add(menuStop);
+                menuMain.add(menuExit);
+                menuAbout.add(menuCredit);
+                menubar.add(menuMain);
+                menubar.add(menuAbout);
+                frame.setJMenuBar(menubar);
+                
+                /* END*08* */
+               
+		SpaceShip v = new SpaceShip(180, 550, 35, 35);
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
+                         
+                /* *09* */
+                MenuListener menuListener = new MenuListener(menuExit,menuCredit,menuRestart,menuStop,engine);
+                menuExit.addActionListener(menuListener);
+                menuCredit.addActionListener(menuListener);
+                menuRestart.addActionListener(menuListener);
+                menuStop.addActionListener(menuListener);
+                /* END*09* */
+                
 		frame.addKeyListener(engine);
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 		
 		engine.start();
 	}
+       
+    
+        
 }
